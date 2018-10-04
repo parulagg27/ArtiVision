c3f51d2bc727fd071948ab0805407013983484ef
Gunnika <b.gunnika@gmail.com>
2018-10-04 18:07:43 +0530

    First Flask app commit

---
+++ WIEHack.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from gtts import gTTS
+import os
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1> Homepage </h1>"
+
+@app.route('/audio',methods= ["POST","GET"])
+def audio():
+    tts = gTTS(text='Testing', lang='en')
+    tts.save("test.mp3")
+    os.system("mpg321 test.mp3")
+    return "<h3> Audio </h3>"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',debug=True)
