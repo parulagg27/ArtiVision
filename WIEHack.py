21de91753a44a8544e7636a8f84a01508ba8f6c5
Gunnika <b.gunnika@gmail.com>
2018-10-04 19:10:35 +0530

    Updated

--- WIEHack.py
+++ WIEHack.py
@@ -12,7 +12,7 @@ def audio():
     tts = gTTS(text='Testing', lang='en')
     tts.save("test.mp3")
     os.system("mpg321 test.mp3")
-    return "<h3> Audio </h3>"
+    return "os.system('mpg321 test.mp3')"
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',debug=True)
